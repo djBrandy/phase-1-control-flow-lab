aaad62950b8b1181220726fc9cf6105cbed41ef9
djBrandy <dandobrandon0@gmail.com>
2024-03-21 22:55:04 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideDistance){
+  let message;
+  if (rideDistance <= 400) {
+    message = 'This one is on me!';
+  }else if (rideDistance >= 400 && rideDistance<= 2000) {
+    message = 'That will be twenty bucks.';
+  }if (rideDistance > 2000 ) {
+    message = 'I will gladly take your thirty bucks.';
+  }if (rideDistance > 2000) {
+    message = 'I will gladly take your thirty bucks.';
+  }if (rideDistance > 2500) {
+    message = 'No can do.';
+  }
+  return message;
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+        return 'Thank you so much.';
+    case 'not as generous':
+        return 'Thank you.';
+    default:
+        return 'Bye.';
+}
 }
